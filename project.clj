cb2bd6df56b89d8cdc52f5efe8ccb4bc6c4b840f
田所 駿佑 <shunsuke.tadokoro@bizreach.co.jp>
2016-07-17 11:01:47 +0900

    Hatch

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject leiningen-sandbox "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+    [org.clojure/math.combinatorics "0.1.3"]
+    [mylib/mylib "0.1.0-SNAPSHOT"]]
+  :main ^:skip-aot leiningen-sandbox.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
